dbc9d81b1c5785c46bc5eb0bca50521359b1b313
freqyXin <maxine.filcher@hotmail.com>
2019-11-22 08:50:25 -0700

    Add files via upload

--- System-Information-Gathering.ps1
+++ System-Information-Gathering.ps1
@@ -4,20 +4,25 @@ Write-Host "This script must be executed locally."
 #input from user
 $YOUR_OUT_LOCATION = Read-Host "Please enter the path to your folder you would like to save results to"
 
+    # Grabs current network interface configuration
     wmic nicconfig list full | Out-File "$YOUR_OUT_LOCATION\nicconfig.txt"    
 
+    #Grabs full list of processes 
 	wmic process list full | Out-File "$YOUR_OUT_LOCATION\process.txt"
 	
-	wmic qfe list full | Out-File "$YOUR_OUT_LOCATION\qfe-patches.txt"
-	
+    #Gives full list of services runing on the system
 	wmic service list full | Out-File "$YOUR_OUT_LOCATION\services.txt"
-
+    
+    #Grabs hot-fix patches applied to system
 	Get-HotFix | Out-File "$YOUR_OUT_LOCATION\hot-fixes.txt"
 
+    #Grabs the system's current firewall rules
 	Get-NetFirewallRule -all | Out-File "$YOUR_OUT_LOCATION\host-firewall-rules.txt"
-
+    
+    #Shows system ports and the processes running on each
 	netstat -nao | Out-File "$YOUR_OUT_LOCATION\ports-processes.txt"
-
+    
+    #Shows system service and the processes associated
 	tasklist /svc | Out-File "$YOUR_OUT_LOCATION\services-processes.txt"
 
 Write-Host "Hope this helped. Thanks for using!"
