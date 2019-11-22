a9ba73caf08f384071b486b81dad87088dd53695
freqyXin <maxine.filcher@hotmail.com>
2019-11-22 08:36:57 -0700

    Add files via upload

---
+++ System-Information-Gathering.ps1
@@ -0,0 +1,23 @@
+﻿Write-Host "Thanks for using this script. It is intended for System Admins and Penetration Testers"
+Write-Host "This script must be executed locally."
+
+#input from user
+$YOUR_OUT_LOCATION = Read-Host "Please enter the path to your folder you would like to save results to"
+
+    wmic nicconfig list full | Out-File "$YOUR_OUT_LOCATION\nicconfig.txt"    
+
+	wmic process list full | Out-File "$YOUR_OUT_LOCATION\process.txt"
+	
+	wmic qfe list full | Out-File "$YOUR_OUT_LOCATION\qfe-patches.txt"
+	
+	wmic service list full | Out-File "$YOUR_OUT_LOCATION\services.txt"
+
+	Get-HotFix | Out-File "$YOUR_OUT_LOCATION\hot-fixes.txt"
+
+	Get-NetFirewallRule -all | Out-File "$YOUR_OUT_LOCATION\host-firewall-rules.txt"
+
+	netstat -nao | Out-File "$YOUR_OUT_LOCATION\ports-processes.txt"
+
+	tasklist /svc | Out-File "$YOUR_OUT_LOCATION\services-processes.txt"
+
+Write-Host "Hope this helped. Thanks for using!"
